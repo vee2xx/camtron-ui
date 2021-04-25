a07132cff93f9c4c6c9d6d875debd61d8de1df4c
Slugsy <slugsy@slugsy.me>
2021-04-25 15:19:09 -0700

    Adding windows binary

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 electron-packager . camtron --platform=linux --arch=x64 --overwrite
-# electron-packager . camtron --platform=win32 --overwrite
+electron-packager . camtron --platform=win32 --overwrite
 # electron-packager . camtron --platform=darwin --arch=x64 --overwrite
 
 rm -rf assets
 mkdir assets
 
 zip -mr assets/camtron-linux-x64.zip camtron-linux-x64/
+zip -mr assets/camtron-win32-x64.zip camtron-win32-x64/
