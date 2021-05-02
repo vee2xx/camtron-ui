9af6c425f017563046cef4277dd0879aab2971c4
Slugsy <slugsy@slugsy.me>
2021-05-01 22:07:06 -0700

    shrinking package

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
-electron-packager . camtron --platform=linux --arch=x64 --overwrite
-electron-packager . camtron --platform=win32 --overwrite
-# electron-packager . camtron --platform=darwin --arch=x64 --overwrite
+npm prune
+
+electron-packager . camtron --platform=linux --arch=x64 --overwrite --asar
+electron-packager . camtron --platform=win32 --overwrite --asar
+electron-packager . camtron --platform=darwin --arch=x64 --overwrite --asar
 
 rm -rf assets
 mkdir assets
 
-zip -mr assets/camtron-linux-x64.zip camtron-linux-x64/
-zip -mr assets/camtron-win32-x64.zip camtron-win32-x64/
+zip -rm  assets/camtron-linux-x64.zip camtron-linux-x64/
+zip -rm  assets/camtron-win32-x64.zip camtron-win32-x64/
+zip -rm assets/camtron-darwin-x64.zip camtron-darwin-x64/
+
+rm -rf camtron-darwin-x64/
+
